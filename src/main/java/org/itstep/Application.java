9e63f7d9b9d6b11a517c1fc4aed8beb7746a99d7
Vladimir <rmastodon0603@gmail.com>
2021-01-04 00:00:18 +0200

    #done hw

--- src/main/java/org/itstep/Application.java
+++ src/main/java/org/itstep/Application.java
@@ -1,7 +1,31 @@
 package org.itstep;
 
+import java.util.*;
+
 public class Application {
+
     public static void main(String[] args) {
-        // Ваш код пишите здесь
+
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Set<String>> networkMap = new HashMap<>();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("TYPE TV NETWORK: ");
+            String channel = scanner.nextLine();
+            System.out.print("Type TV SHOW on " + channel + " ( channel name ): ");
+            String show = scanner.nextLine();
+
+            if (!networkMap.containsKey(channel)){
+                networkMap.put(channel, new TreeSet<>());
+            }
+            networkMap.get(channel).add(show);
+            System.out.println(networkMap);
+            System.out.print("\n");
+        }
+        String[] channels = networkMap.keySet().toArray(new String[0]);
+        Arrays.sort(channels);
+        for(String channel: channels){
+            System.out.println(channel + ":" + networkMap.get(channel));
+        }
     }
 }
